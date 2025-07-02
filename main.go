7c8beb625889c599272787465e06b1af39735510
Jason Mavandi <mvndaai@gmail.com>
2025-07-02 10:18:11 -0600

    note on random

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 func main() {
 	g := sudoku.Game{}
 	err := g.FillBasic(boards.NYTHard2June2025)
-	//err := g.FillBasic(basicEasy)
+	//err := g.FillBasic(boards.BasicEasy)
 	if err != nil {
 		log.Fatalf("Failed to fill game: %v", err)
 	}
 
 	//g.HideSimple = true // Hide basic eliminators
-	//g.RandomEliminators = true // Randomize the order of eliminators
+	//g.RandomEliminators = true // Randomize the order of eliminators TODO this causes errors.
 	g.RunSimpleFirst = true // Run simple eliminators first quietly
 	g.StepThrough()
 }
