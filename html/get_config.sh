5d8c7be6eca164b0cd97f820cc551dcd55bc0758
Jason Mavandi <mvndaai@gmail.com>
2025-08-22 11:43:59 -0600

    make it so the edited config never uploads

--- html/get_config.sh
+++ html/get_config.sh
@@ -3,3 +3,6 @@
 
 API_URL=https://api.github.com/repos/mvndaai/secrets/contents/sudoku_hints.jsonc
 gh api $API_URL -H "Accept: application/vnd.github.raw" > config.jsonc
+
+# make it so this file never gets uploaded to github
+git update-index --assume-unchanged config.jsonc
